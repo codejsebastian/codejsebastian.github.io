5577734adbd5c77f0740d80e0cced24087e9dd44
Javier Sebastián <jsebastian.correo@yahoo.es>
2024-05-04 20:24:55 +0100

    Initial commit

---
+++ script2.js
@@ -0,0 +1,41 @@
+"use strict"
+document.getElementById("search-button").addEventListener("click", consultarAPI);
+
+function consultarAPI() {
+    let xhr, apiKey, city, url;
+    
+    city = document.getElementById("place-input").value; 
+    apiKey = "e53011891a584287b4eb3ab0233ac000";
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+    
+    xhr = new XMLHttpRequest();;
+    xhr.onload = function () {
+        mostrarInformacion(this);
+    }
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function mostrarInformacion(xhr) {
+    let obj, cityName, cityCountry, cityTemp, weatherIcon, weatherDesc, htmlCode;
+
+    console.log(xhr.responseText);
+    obj = JSON.parse(xhr.responseText);
+    cityName = obj.name;
+    cityCountry = obj.sys.country;
+    cityTemp = String(Math.round(obj.main.temp));
+    weatherIcon = "https://openweathermap.org/img/wn/" + obj.weather[0].icon + "@2x.png";
+    weatherDesc = obj.weather[0].description;
+    htmlCode =
+        '<div class="card">' +
+            '<h2 class="city-name">' + cityName + '<sup>' + cityCountry + '</sup></h2>' +
+            '<p class="city-temp">' + cityTemp +'<sup>ºC</sup></p>' +
+            '<figure>' +
+                '<img src="' + weatherIcon + '" alt="">' +
+                '<figcaption>' + weatherDesc + '</figcaption>' +
+            '</figure>' +
+        '</div>';
+    document.getElementById("cards").innerHTML += htmlCode;
+    document.getElementById("place-input").value = "";
+    
+}
